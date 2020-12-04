59b4fc780dddaf00d49c66bde0d5260e26228287
Munjerin Hossain <munjerin15muna@gmail.com>
2020-12-04 21:07:09 +0600

    db changed

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: "mongodb+srv://admin:admintwenty2020@mern-skill-cluster.lfkhw.mongodb.net/MernSKill?retryWrites=true&w=majority"
+  mongoUri: "mongodb+srv://mernadmin:mernadmin123@mern-skill-cluster.be1me.mongodb.net/mern-skill?retryWrites=true&w=majority"
 }
 
 export default config
